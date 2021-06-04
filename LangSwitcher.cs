e1f7e38d2697d40a83b665ee513b69865d3faf07
ArtofGaming <54565543+ArtofGaming@users.noreply.github.com>
2021-06-03 19:57:27 -0700

    Add files via upload

---
+++ LangSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Naninovel;
+
+
+public class LangSwitcher : MonoBehaviour
+{
+    public Button switcher;
+    // Start is called before the first frame update
+    void Start()
+    {
+       switcher.onClick.AddListener(Transition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Transition()
+    {
+        Engine.GetService<ILocalizationManager>().SelectLocaleAsync("de");
+        Debug.Log(Engine.GetService<ILocalizationManager>().SelectedLocale);
+    }
+}
